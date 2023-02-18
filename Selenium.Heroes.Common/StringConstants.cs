fc259c93722887a95ea004ce27f8779d2cf58ad5
Cloxy777 <Kokorin1506@gmail.com>
2023-02-18 14:47:25 +0200

    Worker fixed.

--- Selenium.Heroes.Common/StringConstants.cs
+++ Selenium.Heroes.Common/StringConstants.cs
@@ -6,11 +6,11 @@ public class StringConstants
 
     public const string CardGameFileName = "board.png";
 
-    public const string ManualCardEffectsFullPath = "E:\\VisualStudio\\Projects\\Selenium\\manual_card_effects.json";
+    public const string ManualCardEffectsFullPath = "E:\\GitHub\\Selenium\\manual_card_effects.json";
 
-    public const string GeneratedCardEffectsFullPath = "E:\\VisualStudio\\Projects\\Selenium\\generated_card_effects.json";
+    public const string GeneratedCardEffectsFullPath = "E:\\GitHub\\Selenium\\generated_card_effects.json";
 
-    public const string CardsFullPath = "E:\\VisualStudio\\Projects\\Selenium\\cards.json";
+    public const string CardsFullPath = "E:\\GitHub\\Selenium\\cards.json";
 
-    public const string CardsFullPathDirectory = "E:\\VisualStudio\\Projects\\Selenium\\Cards";
+    public const string CardsFullPathDirectory = "E:\\GitHub\\Selenium\\Cards";
 }
